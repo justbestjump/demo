d8c339f47b1abe0ab2dfa609c1f9cceca96573f5
fgw <1980260252@qq.com>
2020-11-27 10:16:24 +0800

    dev的第1次提交

--- src/main/java/jpa/dao/UserMapper.java
+++ src/main/java/jpa/dao/UserMapper.java
@@ -14,5 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserMapper extends Mapper<User>{
-
+    public static void main(String[] args) {
+        System.out.println("dasd");
+    }
 }
